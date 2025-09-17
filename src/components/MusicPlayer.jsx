0ca6ee4e20d5f7abd9999ea0d1bc49de2d06f1b1
jaylenperez <jaylenestella24@gmail.com>
2025-09-16 21:07:54 -0500

    song title

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,9 +1,11 @@
 import CoverArt from "./CoverArt";
+import SongTitle from "./SongTitle";
 
 export default function MusicPlayer() {
   return (
     <div className="mx-auto w-full max-w-xl p-6">
       <CoverArt />
+      <SongTitle />
     </div>
   );
 }
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,8 @@
+export default function SongTitle() {
+  return (
+    <div>
+      <h1 className="font-sans text-3xl font-bold">Song Title</h1>
+      <p className="font-sans text-lg font-bold text-gray-500">Song Artist</p>
+    </div>
+  );
+}
