4947f89d4d0be18528ff33a2a7e8b340e404c822
jaylenperez <jaylenestella24@gmail.com>
2025-09-24 17:13:53 -0500

    responsive layout

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -3,7 +3,7 @@ import Playlist from "./Playlist";
 
 export default function MusicPlayer() {
   return (
-    <div className="mx-auto flex w-full max-w-5xl gap-8 p-6">
+    <div className="mx-auto flex w-full max-w-5xl flex-col gap-8 p-6 md:flex-row">
       <div className="flex-1">
         <CurrentlyPlaying />
       </div>
